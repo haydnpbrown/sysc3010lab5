d942c6ef01ee246e797783ccfb5fbe0d19ed2817
haydnpbrown <haydnbrown@cmail.carleton.ca>
2019-10-07 10:36:54 -0400

    commit #3 - final commit

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.lab5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.lab5.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /** Called when the user taps the advance bar button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText8)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
 }
